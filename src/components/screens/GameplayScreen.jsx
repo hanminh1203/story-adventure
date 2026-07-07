a9e31f2612f31200d4bcf277b88573becfd41f68
Mark Tran <tranhanminh34@gmail.com>
2026-07-07 12:32:15 +0800

    Move the Leave button

--- src/components/screens/GameplayScreen.jsx
+++ src/components/screens/GameplayScreen.jsx
@@ -100,13 +100,32 @@ export default function GameplayScreen({ active, character, onFinalize, onExit }
           </div>
         </div>
 
+        <button
+          id="exit-btn"
+          className="btn-glass exit-btn"
+          type="button"
+          aria-label={UI_TEXT.EXIT_BTN_ARIA_LABEL}
+          onClick={showExitConfirm}
+        >
+          <span className="exit-btn-icon" aria-hidden="true">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path d="M0 0 C5.28 0 10.56 0 16 0 C16 2.64 16 5.28 16 8 C15.34 8 14.68 8 14 8 C14 6.02 14 4.04 14 2 C12.68 2 11.36 2 10 2 C10 7.61 10 13.22 10 19 C11.32 19 12.64 19 14 19 C14 17.02 14 15.04 14 13 C14.66 13 15.32 13 16 13 C16 15.64 16 18.28 16 21 C14.02 21 12.04 21 10 21 C10 21.99 10 22.98 10 24 C8.52031676 23.85950482 7.04125484 23.71245174 5.5625 23.5625 C4.73878906 23.48128906 3.91507812 23.40007813 3.06640625 23.31640625 C1 23 1 23 0 22 C-0.08700342 20.36527118 -0.10701063 18.72685945 -0.09765625 17.08984375 C-0.09443359 16.10048828 -0.09121094 15.11113281 -0.08789062 14.09179688 C-0.07951172 13.05087891 -0.07113281 12.00996094 -0.0625 10.9375 C-0.05798828 9.89271484 -0.05347656 8.84792969 -0.04882812 7.77148438 C-0.03699927 5.18096546 -0.01906319 2.59047578 0 0 Z M2 2 C2 7.94 2 13.88 2 20 C3.98 20.66 5.96 21.32 8 22 C8 16.06 8 10.12 8 4 C6.02 3.34 4.04 2.68 2 2 Z " transform="translate(0,0)"/>
+              <path d="M0 0 C2.86724686 0.57344937 3.8614515 0.8614515 6 3 C5.63318342 5.93453261 5.13562754 6.86437246 3 9 C2.01 9 1.02 9 0 9 C0 8.34 0 7.68 0 7 C-1.65 6.67 -3.3 6.34 -5 6 C-5 5.01 -5 4.02 -5 3 C-3.35 2.67 -1.7 2.34 0 2 C0 1.34 0 0.68 0 0 Z " transform="translate(18,6)"/>
+            </svg>
+          </span>
+        </button>
+
         <div id="progress-trail" className="progress-trail" aria-label={UI_TEXT.PROGRESS_TRAIL_ARIA_LABEL}>
           <div id="progress-trail-label" className="progress-trail-label">
             {locations.length > 0
               ? formatTemplate(UI_TEXT.STOP_PANEL_TEMPLATE, {
-                  current: currentIndex + 1,
-                  total: locations.length,
-                })
+                current: currentIndex + 1,
+                total: locations.length,
+              })
               : ""}
           </div>
           <div id="progress-trail-dots" className="progress-trail-dots">
@@ -170,16 +189,6 @@ export default function GameplayScreen({ active, character, onFinalize, onExit }
             </div>
           </div>
         </div>
-
-        <button
-          id="exit-btn"
-          className="btn-glass exit-btn"
-          type="button"
-          aria-label={UI_TEXT.EXIT_BTN_ARIA_LABEL}
-          onClick={showExitConfirm}
-        >
-          {UI_TEXT.EXIT_BTN_TEXT}
-        </button>
 
         <div
           id="location-ui"
